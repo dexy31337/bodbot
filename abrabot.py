767fdf9e88f3838eec7ac73d0b46293906851a2b
Timur Malin <dexy.3133@gmail.com>
2022-07-05 15:00:11 +0100

    More text edits

--- abrabot.py
+++ abrabot.py
@@ -11,7 +11,7 @@ bot = Bot(token=TOKEN, api_url_base=API_URL_BASE)
 def message_cb(bot, event):
     print(event)
     if event.data['chat']['type'] == "private" and not event.text.startswith('/'): 
-        bot.send_text(chat_id=event.from_chat, text=event.data['from']['firstName']+", cпасибо за обращение.\nСейчас я найду специалиста БОД, который вам поможет!")
+        bot.send_text(chat_id=event.from_chat, text=event.data['from']['firstName']+", cпасибо за обращение.\nБОД спешит на помощь!")
         msgtext = "Cообщение от @[" + event.data['from']['userId'] + "]\n\n" + event.text
         bot.send_text(chat_id=CHATID, text=msgtext)
     if event.data['chat']['type'] == "private" and event.text == "/start":
